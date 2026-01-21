9a92f5266c420fd2f6fc51647eddcc40773b5034
Jose Antonio Morales Gutierrez <josea.morgut.1@educa.jcyl.es>
2026-01-21 12:27:49 +0100

    Ejercicio sobres ArrayList y su funcionamiento

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,24 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-void main() {
-  //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-  // to see how IntelliJ IDEA suggests fixing it.
-  IO.println(String.format("Hello and welcome!"));
+import java.util.ArrayList;
+import java.util.List;
 
-  for (int i = 1; i <= 5; i++) {
-    //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-    // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-    IO.println("i = " + i);
-  }
+public  class Main {
+    public static void main(String[] args) {
+        //Creamos una lista (List) de nombres
+        List<String> nombres = new ArrayList<String>();
+        //Añadimos varios nombres
+        nombres.add("Juan");
+        nombres.add("Marco");
+        nombres.add("Pedro");
+        nombres.add("Juan");
+        nombres.add("Marco");
+
+        //Eliminamos algunos nombres
+        nombres.remove("Pedro");
+        nombres.remove("Marco");
+
+        //Con un for-each recorremos la lista y mostramos los nombres por pantalla
+        for(String nombre : nombres){
+            System.out.println(nombre);
+        }
+    }
 }
